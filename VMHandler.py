c0606bc7bf3ce6d32693b184fd8baee812013589
RonnyPastor <116033524+RonnyPastor@users.noreply.github.com>
2023-06-25 13:16:38 -0500

    uuwu

--- VMHandler.py
+++ VMHandler.py
@@ -6,7 +6,9 @@ from Classes.Network import Network
 class VMConstructor:
     def createVM(VM: VM,networks,neutron: NeutronClient,nova: NovaClient):
         ##Networks es una listas con los nombres de las redes a las cuales se deben crear
-        nova.create_instance(flavor_id=,name=,keypairID=,network_id=,securitygroupID=)
-        pass
-    def editVM(VM,networks,neutron,nova):
+        #nova.create_instance_v2(flavor_id=VM.flavorID,name=VM.name,keypairID=VM.keyPairID,network_id=networks,securitygroupID=VM.securitygroupID)
+
+
+    def editVM(VM: VM,network,neutron: NeutronClient,nova: NovaClient):
+        
         pass
